0d96255dad47f44e15e672af880422125307aa1e
CHENCHO121 <chenchogyeltshen000@gmail.com>
2021-09-26 13:27:55 +0600

    day 10 completed

--- codepur_1/lib/models/catalog.dart
+++ codepur_1/lib/models/catalog.dart
@@ -1,18 +1,8 @@
-class Item{
 
-  final String id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
+class CatalogModel{
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-
-}
-
-final products=[Item(
-  id: "Codepur001",
+  static final items=[Item(
+  id: 1,
   name: "Iphone Pro 11",
   desc:"Apple Iphone 11th generartion",
   price: 999,
@@ -20,3 +10,22 @@ final products=[Item(
   image: "https://images-na.ssl-images-amazon.com/images/I/51I7B68Vd-L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
 
 )];
+
+}
+
+
+
+class Item{
+
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+}
+
+
--- codepur_1/lib/pages/home_page.dart
+++ codepur_1/lib/pages/home_page.dart
@@ -1,18 +1,27 @@
+import 'package:codepur_1/models/catalog.dart';
 import 'package:codepur_1/widgets/drawer.dart';
+import 'package:codepur_1/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+   final int days = 30;
+    final String name = "WebNet";
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "WebNet";
+    final dummyList=List.generate(20, (index) => CatalogModel.items[0]);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days day Flutter by $name"),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index){
+          return ItemWidget(item: dummyList[index],);
+
+        },
         ),
       ),
       drawer: MyDrawer(),
