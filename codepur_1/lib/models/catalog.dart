b121c83cdc6be1f9b830b18656e3fdeb6e870252
CHENCHO121 <chenchogyeltshen000@gmail.com>
2021-09-26 12:24:36 +0600

    day 9 completed

---
+++ codepur_1/lib/models/catalog.dart
@@ -0,0 +1,22 @@
+class Item{
+
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+
+}
+
+final products=[Item(
+  id: "Codepur001",
+  name: "Iphone Pro 11",
+  desc:"Apple Iphone 11th generartion",
+  price: 999,
+  color: "#9E776F",
+  image: "https://images-na.ssl-images-amazon.com/images/I/51I7B68Vd-L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+
+)];
